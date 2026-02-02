913544dad320a93a8fbefcab249771aa643a7b2c
risper4 <risperwanjikugichia@gmail.com>
2026-02-02 15:18:46 +0300

    Added updated js

---
+++ script.js
@@ -0,0 +1,9 @@
+const birthDate = document.getElementById('Birthdate')
+const gender = document.getElementById('Gender')
+let form = document.getElementById('form')
+
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault()
+    
+})
